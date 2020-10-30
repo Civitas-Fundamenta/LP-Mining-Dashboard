f9ca2e89fea98006a581d431edd0a06f76ad1c48
FallenGravity <ethanvb55@gmail.com>
2020-10-30 21:21:43 +0200

    Import Web3 and export it as an API

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,12 @@
 import Vue from 'vue';
 import axios from 'axios';
+import Web3 from 'web3';
 
+const API = {};
+
+API.Web3 = new Web3(
+  new Web3.providers.HttpProvider('https://api.infura.io/v1/jsonrpc/mainnet'),
+);
+
+Vue.prototype.$API = API;
 Vue.prototype.$axios = axios;
