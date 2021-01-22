1a0764348b2d72d60bc7c9e80b806f899aed10a4
Ethan van Ballegooyen <ethanvb55@gmail.com>
2021-01-22 22:40:51 +0200

    Patch Stats

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -5,7 +5,7 @@ import Web3 from 'web3';
 const API = {};
 
 API.Web3 = new Web3(
-  new Web3.providers.HttpProvider('https://api.infura.io/v1/jsonrpc/mainnet'),
+  new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/9b35dedcca93433d8cba4b8dd762ed45'),
 );
 
 Vue.prototype.$API = API;
