924b1787497e23d8c00355e7f2c175266b609691
hooftly <mhooft@tf-dev.org>
2021-05-18 09:21:39 -0600

    add Staking and Mining Art

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,14 +6,14 @@
           <i class="fas fa-coins fa-10x" style="padding: 25%;"></i>
           <q-card-section>
             <a href="/#/Mining">
-            <q-btn color="white" text-color="black" label="Liquidity Mining" />
+            <img src="../assets/miningPNG.png"
             </a>
           </q-card-section>
         </q-card>
       </div>
       <div class="col-xs-12 col-sm-6" style="padding: 2%;">
         <q-card flat bordered class="cbg">
-          <i class="fas fa-money-bill-wave fa-10x" style="padding: 25%;"></i>
+          <img src="../assets/stakingPNG.png">
         <q-card-section>
           <a href="/#/Staking">
             <q-btn color="white" text-color="black" label="Token Staking" />
